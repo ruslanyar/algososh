916f9d5aec7a6ae250a9bdbf5f4a215c2ccf25d5
ruslanyar <styxrus@yandex.ru>
2022-09-19 16:17:09 +0300

    refactor(sorting): separate logic of algorithm from rendering

--- src/components/sorting-page/sorting-algo.ts
+++ src/components/sorting-page/sorting-algo.ts
@@ -1,12 +1,4 @@
-import { SetStateAction } from 'react';
-
-import {
-  MIN_LENGTH,
-  MAX_LENGTH,
-  MIN_VALUE,
-  MAX_VALUE,
-} from './constants';
-import { setDelay, swap } from '../../utils/utils';
+import { swap } from '../../utils/utils';
 
 import { Direction } from '../../types/direction';
 import { ElementStates } from '../../types/element-states';
@@ -16,12 +8,17 @@ const getRandomInRange = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-export const getRandomArr = () => {
-  const arrLength = getRandomInRange(MIN_LENGTH, MAX_LENGTH);
+export const getRandomArr = (
+  minLength: number,
+  maxLength: number,
+  minValue: number,
+  maxValue: number
+) => {
+  const arrLength = getRandomInRange(minLength, maxLength);
   const randomArr: TElement[] = [];
   let randomNum: number;
   while (randomArr.length < arrLength) {
-    randomNum = getRandomInRange(MIN_VALUE, MAX_VALUE);
+    randomNum = getRandomInRange(minValue, maxValue);
     // eslint-disable-next-line no-loop-func
     if (!randomArr.some((el) => randomNum === el.value)) {
       randomArr.push({ value: randomNum, state: ElementStates.Default });
@@ -35,7 +32,7 @@ const setElementsStateSelection = (
   idxI: number = arr.length,
   idxJ: number = arr.length,
   selectedIdx: number = arr.length
-) => {
+): TElement[] => {
   let state: ElementStates;
   const newArray = arr.map((el, idxEl) => {
     if (idxEl === selectedIdx || idxEl === idxJ) {
@@ -50,34 +47,40 @@ const setElementsStateSelection = (
       state,
     };
   });
+
   return newArray;
 };
 
-export const selectionSort = async (
+export const selectionSort = (
   arr: TElement[],
-  direction: Direction,
-  setFn: React.Dispatch<SetStateAction<TElement[]>>
-) => {
+  direction: Direction
+): TElement[][] | null => {
   const { length } = arr;
+  if (!length) return null;
+
+  const sortMatrix: TElement[][] = [];
+  let stepArray: TElement[];
+
   for (let i = 0; i < length - 1; i++) {
     let selectedIdx = i;
-    if (direction === Direction.Ascending) {
-      for (let j = i + 1; j < length; j++) {
-        await setDelay(1000);
-        setFn(setElementsStateSelection(arr, i, j, selectedIdx));
-        if (arr[selectedIdx].value > arr[j].value) selectedIdx = j;
-      }
-    } else {
-      for (let j = i + 1; j < length; j++) {
-        await setDelay(1000);
-        setFn(setElementsStateSelection(arr, i, j, selectedIdx));
-        if (arr[selectedIdx].value < arr[j].value) selectedIdx = j;
-      }
+
+    for (let j = i + 1; j < length; j++) {
+      stepArray = setElementsStateSelection(arr, i, j, selectedIdx);
+      sortMatrix.push(stepArray);
+      if (
+        direction === Direction.Ascending
+          ? arr[selectedIdx].value > arr[j].value
+          : arr[selectedIdx].value < arr[j].value
+      )
+        selectedIdx = j;
     }
+
     if (arr[i].value !== arr[selectedIdx].value) swap(arr, i, selectedIdx);
   }
-  await setDelay(1000);
-  setFn(setElementsStateSelection(arr));
+  stepArray = setElementsStateSelection(arr);
+  sortMatrix.push(stepArray);
+
+  return sortMatrix;
 };
 
 const setElementsStateBubble = (
@@ -100,30 +103,34 @@ const setElementsStateBubble = (
       state,
     };
   });
+
   return newArray;
 };
 
-export const bubbleSort = async (
+export const bubbleSort = (
   arr: TElement[],
-  direction: Direction,
-  setFn: React.Dispatch<SetStateAction<TElement[]>>
-) => {
+  direction: Direction
+): TElement[][] | null => {
   const { length } = arr;
+  if (!length) return null;
+
+  const sortMatrix: TElement[][] = [];
+  let stepArray: TElement[];
+
   for (let i = 0; i < length; i++) {
-    if (direction === Direction.Ascending) {
-      for (let j = 0; j < length - i - 1; j++) {
-        await setDelay(1000);
-        setFn(setElementsStateBubble(arr, j, i));
-        if (arr[j].value > arr[j + 1].value) swap(arr, j, j + 1);
-      }
-    } else {
-      for (let j = 0; j < length - i - 1; j++) {
-        await setDelay(1000);
-        setFn(setElementsStateBubble(arr, j, i));
-        if (arr[j].value < arr[j + 1].value) swap(arr, j, j + 1);
-      }
+    for (let j = 0; j < length - i - 1; j++) {
+      stepArray = setElementsStateBubble(arr, j, i);
+      sortMatrix.push(stepArray);
+      if (
+        direction === Direction.Ascending
+          ? arr[j].value > arr[j + 1].value
+          : arr[j].value < arr[j + 1].value
+      )
+        swap(arr, j, j + 1);
     }
   }
-  await setDelay(1000);
-  setFn(setElementsStateBubble(arr));
+  stepArray = setElementsStateBubble(arr);
+  sortMatrix.push(setElementsStateBubble(arr));
+
+  return sortMatrix;
 };
--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -8,6 +8,8 @@ import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { Direction } from '../../types/direction';
 import { bubbleSort, getRandomArr, selectionSort } from './sorting-algo';
 import { TElement } from './types';
+import { MIN_LENGTH, MAX_LENGTH, MIN_VALUE, MAX_VALUE } from './constants';
+import { DELAY_IN_MS } from '../../constants/delays';
 
 import styles from './sorting-page.module.css';
 
@@ -22,7 +24,12 @@ export const SortingPage: React.FC = () => {
   }, []);
 
   const getNewArray = () => {
-    const randomArr = getRandomArr();
+    const randomArr = getRandomArr(
+      MIN_LENGTH,
+      MAX_LENGTH,
+      MIN_VALUE,
+      MAX_VALUE
+    );
     setArray(randomArr);
   };
 
@@ -30,24 +37,50 @@ export const SortingPage: React.FC = () => {
     setIsSelectionChecked((prev) => !prev);
   };
 
-  const ascHandler = async () => {
+  const ascHandler = () => {
     setIsAscLoading(true);
-    if (isSelectionChecked) {
-      await selectionSort(array, Direction.Ascending, setArray);
-    } else {
-      await bubbleSort(array, Direction.Ascending, setArray);
-    }
-    setIsAscLoading(false);
+
+    const matrix = isSelectionChecked
+      ? selectionSort(array, Direction.Ascending)
+      : bubbleSort(array, Direction.Ascending);
+
+    if (!matrix) return setIsAscLoading(false);
+
+    let step = 0;
+    setArray(matrix[step]);
+
+    const timerId = setInterval(() => {
+      if (step < matrix.length - 1) {
+        step++;
+        setArray(matrix[step]);
+      } else {
+        clearInterval(timerId);
+        setIsAscLoading(false);
+      }
+    }, DELAY_IN_MS);
   };
 
-  const descHandler = async () => {
+  const descHandler = () => {
     setIsDescLoading(true);
-    if (isSelectionChecked) {
-      await selectionSort(array, Direction.Descending, setArray);
-    } else {
-      await bubbleSort(array, Direction.Descending, setArray);
-    }
-    setIsDescLoading(false);
+
+    const matrix = isSelectionChecked
+      ? selectionSort(array, Direction.Descending)
+      : bubbleSort(array, Direction.Descending);
+
+    if (!matrix) return setIsDescLoading(false);
+
+    let step = 0;
+    setArray(matrix[step]);
+
+    const timerId = setInterval(() => {
+      if (step < matrix.length - 1) {
+        step++;
+        setArray(matrix[step]);
+      } else {
+        clearInterval(timerId);
+        setIsDescLoading(false);
+      }
+    }, DELAY_IN_MS);
   };
 
   return (
